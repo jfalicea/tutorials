8c91247194e0cb3f4e863f592e3598d6ae5f1e7f
Jonathan <jonathan@boldideas.net>
2020-01-27 23:14:32 -0500

    used express .sendFile() method

--- nodeTutorials/section-4/assignment/app.js
+++ nodeTutorials/section-4/assignment/app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const path = require('path')
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
@@ -10,7 +11,8 @@ app.use(shopRoutes);
 
  //404 Page 
  app.use((req, res, next) =>{
-     res.status(404).send('<h1>Page Not found</h1>')
+    //  res.status(404).send('<h1>Page Not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'), (err)=>{if (err) throw err} )
  })
 
 
--- nodeTutorials/section-4/assignment/routes/admin.js
+++ nodeTutorials/section-4/assignment/routes/admin.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const router = express.Router(); 
-
-
+const path = require('path');
+const rootDir = require('../utils/path')
 router.use('/add-product',(req,res,next)=>{
     // console.log('hello-world & /users');
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title" /><button type="submit">Send</button></form>')
+    // res.send('<form action="/admin/product" method="POST"><input type="text" name="title" /><button type="submit">Send</button></form>')
+    res.sendFile(path.join(rootDir,"views","add-product.html"))
 })
 
 router.post('/product', (req,res,next)=>{
